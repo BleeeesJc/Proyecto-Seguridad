a4e5b042239f1b9c9db77da81a2e6f53fa662c32
Mateo Valenzuela <valenzuelamateo98@gmail.com>
2025-05-23 21:33:29 -0400

    segunda prueba render

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,32 +1,16 @@
-// server/src/config/db.js
 const { Sequelize } = require('sequelize');
 
-// Si existe DATABASE_URL, lo usamos; si no, caemos en modo "local"
-const connectionString = process.env.DATABASE_URL;
-
-const sequelize = connectionString
-  ? new Sequelize(connectionString, {
-      dialect: 'postgres',
-      protocol: 'postgres',
-      logging: false,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      }
-    })
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        logging: false
-      }
-    );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
 
 sequelize
   .authenticate()
