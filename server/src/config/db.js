183b19107a23269614b34830ce93efc62925690d
Mateo Valenzuela <valenzuelamateo98@gmail.com>
2025-05-23 22:00:21 -0400

    esta mas

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,23 +1,14 @@
 // server/src/config/db.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false }
   }
-);
+});
 
 sequelize
   .authenticate()
